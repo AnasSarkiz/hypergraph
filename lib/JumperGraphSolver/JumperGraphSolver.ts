6f0869e8ebf56b6241c08a0c8223db02d367aad4
seveibar <seveibar@gmail.com>
2026-01-06 13:50:03 -0800

    calibration 50 -> 58% single 1206x4

--- lib/JumperGraphSolver/JumperGraphSolver.ts
+++ lib/JumperGraphSolver/JumperGraphSolver.ts
@@ -25,23 +25,23 @@ export class JumperGraphSolver extends HyperGraphSolver<JRegion, JPort> {
       ...input,
       greedyMultiplier: 1.2,
       rippingEnabled: true,
-      ripCost: 100,
+      ripCost: 40,
     })
-    this.MAX_ITERATIONS = 4000 + input.inputConnections.length * 500
+    this.MAX_ITERATIONS = 400 + input.inputConnections.length * 200
   }
 
   override estimateCostToEnd(port: JPort): number {
     return distance(port.d, this.currentEndRegion!.d.center)
   }
   override getPortUsagePenalty(port: JPort): number {
-    return (port.ripCount ?? 0) * 2
+    return port.ripCount ?? 0
   }
   override computeIncreasedRegionCostIfPortsAreUsed(
     region: JRegion,
     port1: JPort,
     port2: JPort,
   ): number {
-    return computeDifferentNetCrossings(region, port1, port2) * 10
+    return computeDifferentNetCrossings(region, port1, port2) * 6
   }
 
   override getRipsRequiredForPortUsage(
